8d1df51656af3984cbe73352a37fa9fed7e691ae
bensaid <houssembensai16@gmail.com>
2022-09-13 18:12:09 +0100

    new

---
+++ somme.py
@@ -0,0 +1,13 @@
+x=int(input("donner le valeur de x:"))
+y=int(input("donner le valeur de y:"))
+operation=(input("donner l'operation "))
+    
+if("operation==+"):
+    (z=x+y)
+if("operation==*"):
+    (z=x*y)
+    
+print("le resultat est",z)
+ 
+    
+      
